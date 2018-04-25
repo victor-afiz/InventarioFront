39830d960e4686e08eec4fe29f2656f567d6b9d6
IvanGonzalezMartin <kinokillo@gmail.com>
2018-04-25 11:31:03 +0200

    Cabecera Pcc beta

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './general.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header col-sm-6">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
