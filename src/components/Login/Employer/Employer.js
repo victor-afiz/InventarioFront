f23c10c57553aa4c6b16c65638164c40f9ff4fdd
IvanGonzalezMartin <kinokillo@gmail.com>
2018-05-14 11:43:09 +0200

    empoyer and manager

--- src/components/Login/Employer/Employer.js
+++ src/components/Login/Employer/Employer.js
@@ -4,15 +4,22 @@ import './Employer.css';
 
 class Employer extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
     render() {
         return(
+
                 <div id="menu1" className="container tab-pane active form-group">
                     <form className="forum">
-                        <input type="email" className="form-control" placeholder="Email*" id="email" />
+                        <input type="email"  className="form-control" placeholder="Email*" id="email" />
                         <br/>
                         <input type="password" className="form-control" placeholder="Contraseña*" name="name" />
                         <br/>
-                        <input type="submit" className="btn btn-warning btn-block" value="Entrar" />
+                        <input type="submit" onPress={this.handleClick} className="btn btn-warning btn-block" value="Entrar" />
+                        <br/>
                     </form>
                 </div>
         );
